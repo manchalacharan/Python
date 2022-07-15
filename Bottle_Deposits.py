413d0f5f50b60e1c3494de98aca18d661c10a7c9
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-15 20:21:23 +0530

    Update Bottle_Deposits.py

--- Bottle_Deposits.py
+++ Bottle_Deposits.py
@@ -1,4 +1,8 @@
-#
+'''Small deposit is add drink containers to encourage people to recycle them
+    Drink container holding one liter or less have a $0.10 deposit
+    Drink container holding more then one liter or less have a $0.10 deposit
+Program should continue by computing and display the refund that will be received
+for returning those containers'''
 
 bottle_above_liter = int(input("Enter the no of bottle(above it): "))
 bottle_below_liter = int(input("Enter the no of bottle(below it): "))
