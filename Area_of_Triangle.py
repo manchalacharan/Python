3beee1eb3cbcf583fb6c46a6611a9c47b58c0a4e
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-15 21:28:46 +0530

    Add files via upload

---
+++ Area_of_Triangle.py
@@ -0,0 +1,12 @@
+'''The area of triangle formula is
+        area = (b*h)/2
+    where b is length
+          h is height
+    to display the area of triangle value'''
+
+length = int(input("Enter the b value: "))
+height = int(input("Enter the h value: "))
+
+area = (length*height)/2
+
+print("Area of a Triangle is: ",area)
