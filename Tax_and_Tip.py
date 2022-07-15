305c960cfc589019c71373631ba9df96d6e23a55
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-15 20:30:45 +0530

    Update Tax_and_Tip.py

--- Tax_and_Tip.py
+++ Tax_and_Tip.py
@@ -1,6 +1,11 @@
+'''the cost of meals ordered a restaurant from the user
+    computing 18% of tax and tip
+the grand totalfor meal including both tax and the tip.'''
 
 price = int(input("Enter a amount : "))
-#taxp = float(input("Enter the tax percentage: "))
+
+#tax1 = float(input("Enter the tax percentage: "))         #you can choose tax
+#tip1 = float(input("Enter the tip percentage: "))         #you can choose tip
 tax = price*0.18
 tip = price*0.18
 
