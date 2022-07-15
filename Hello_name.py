0de3d678266eda9704f9683e5f436fe48131e4e6
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-15 18:58:07 +0530

    Update Hello_name.py

--- Hello_name.py
+++ Hello_name.py
@@ -1,5 +1,7 @@
-''' User given name then respond say Hello to user name 
+''' User given name then respond say Hello include user name also 
     Eg : Enetr your name : Charan
-         output gives : Hello Charan''' 
+         output gives : Hello Charan '''
+
 a = str(input('Enetr name : '))
+
 print("Hello",a)
