cd10049ae29e520448cd60b9729e86c9d085403f
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-15 20:35:16 +0530

    Update Sum_of_the_first_n_positive_integers.py

--- Sum_of_the_first_n_positive_integers.py
+++ Sum_of_the_first_n_positive_integers.py
@@ -1,3 +1,6 @@
+''' Positive integer n,sum of all of the integer from 1 to n
+    then sum of the first n positive integer can be computing using formula
+                sum = n*(n+1)/2'''
 
 n = int(input("Enter a n Value: "))
 
