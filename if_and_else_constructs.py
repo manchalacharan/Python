f6e74ca7b7560872603fb2e072451576f5d44ed6
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-18 13:59:15 +0530

    Add files via upload

---
+++ if_and_else_constructs.py
@@ -0,0 +1,9 @@
+''' Weather a number is even using
+    if constructs (or) if if statment either else constructs '''
+
+n  = int(input("Enetr a number: "))
+
+if (n%2)==0:
+    print("Even number.")
+else:
+    print("Odd number.")
---
+++ if_constructs.py
@@ -0,0 +1,6 @@
+''' Weather a number is even using if constructs (or) if if statment'''
+
+n  = int(input("Enetr a number: "))
+
+if (n%2)==0:
+    print("Even number.")
