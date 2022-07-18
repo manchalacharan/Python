58d39432fb31d8432f2b8b32de3acda579b729d6
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-18 14:44:18 +0530

    Add files via upload

---
+++ Alphabet_of_vowel_or_consonant_letters.py
@@ -0,0 +1,10 @@
+'''
+    The alphabet form the user a,e,i,o,u to display on the vowel
+    then another alphabet unsing show ERRoR message '''
+
+alp = str(input("Enetr a Alphabet: "))
+
+if(alp=='a')or(alp=='e')or(alp=='i')or(alp=='o')or(alp=='u'):
+    print("This is a Vowel Alphabet.")
+else:
+    print("Error.")
