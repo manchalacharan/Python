0bb778709370d6a58345c0b923ddea64ac308e6b
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-28 19:53:48 +0530

    Add files via upload

---
+++ lower_case_letter_to_convert_upper_case_letter.py
@@ -0,0 +1,24 @@
+''' Any string name to combination of upper case letter and lower case letter
+    this program has print only upper case letters '''
+
+x = input("Enter a name is: ")
+
+upper_case = x.upper()
+print(upper_case)
+
+upper_case = x.isupper()
+print(upper_case)
+
+''' Any string name to combination of upper case letter and lower case letter
+    this program has print only lower case letters 
+
+x = input("Enter a name is: ")
+
+lower_case = x.lower()
+print(lower_case)
+
+lower_case = x.islower()
+print(lower_case)
+
+'''
+
