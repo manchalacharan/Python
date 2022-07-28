d86cd24617e73ecd4d0b4777c07d7ba2b1bfd03a
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-28 22:04:44 +0530

    Update Dicitionary_using_average_marks.py

--- Dicitionary_using_average_marks.py
+++ Dicitionary_using_average_marks.py
@@ -1,23 +1,23 @@
 '''marks using dicitinoary datatype '''
 
-marks = {"eng" : 60, "hin" : 75, "math" : 85, "phy" : 65, "che" : 65}
-roll = [10,25,36,42,50]  
+tenth = {"Telugu" : 9, "English" : 5, "Mathematics" : 10, "Science" : 8, "Social" : 7, "Hindi" : 8}
+inter = [132,148,128,77,54,45,29,26]  
 # loop to sum all values 
 dic = 0
 lis = 0
-for i in marks.values():
+for i in tenth.values():
     dic += i
 
-for j in roll:
+for j in inter:
     lis += i
   
 # using len() to get total keys for mean computation
-dic = dic / len(marks)
-lis = sum(roll)/len(roll)
+dic = dic / len(tenth)
+lis = sum(inter)/len(inter)
 
 # printing result 
-print("The computed mean : " + str(dic))
-print("The computed mean : " + str(lis))
+print("The Average marks is : " + str(dic))
+print("The Average marks is : ",lis)
 
 """
 marks = {"eng" : 60, "hin" : 75, "math" : 85, "phy" : 65, "che" : 65}
