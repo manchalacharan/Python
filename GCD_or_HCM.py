85797a936bf3f6471c26f2a075bf087a55a60c8f
Charan Raj <88527168+manchalacharan@users.noreply.github.com>
2022-07-30 18:57:04 +0530

    Add files via upload

---
+++ GCD_or_HCM.py
@@ -0,0 +1,12 @@
+'''Greastest common divisor'''
+
+n = int(input("Enter a number1 is: "))
+m = int(input("Enter a number2 is: "))
+x = min(x,y)
+hcf = 0
+
+for i in range(1,x+1):
+    if n%i == 0 and m%i == 0:
+        hcf = i
+        
+print("The Greatest common divison is: ", hcf) 
